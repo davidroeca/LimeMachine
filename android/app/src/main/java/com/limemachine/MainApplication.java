ffa254f828e1416e4e552d31702edddde1cc3f94
davidroeca <david.roeca@gmail.com>
2017-10-29 23:40:34 -0400

    add realm to project

--- android/app/src/main/java/com/limemachine/MainApplication.java
+++ android/app/src/main/java/com/limemachine/MainApplication.java
@@ -3,6 +3,7 @@ package com.limemachine;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.rnfs.RNFSPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new VectorIconsPackage(),
             new RNSoundPackage(),
             new RNFSPackage()
